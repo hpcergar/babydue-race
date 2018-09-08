fbdfd63a6b7ae15adb91f6e791396b8b718a0372
hpcer <hpcer@gmail.com>
2018-09-09 01:16:27 +0200

    [IMP] Hard route ok

--- game/src/providers/Decoration.js
+++ game/src/providers/Decoration.js
@@ -8,7 +8,6 @@ export default class {
         map.createFromObjects(layerName, 'Scarecrow', 'autumn-objects', 20, true, false, layer);
         map.createFromObjects(layerName, 'Tree', 'autumn-objects', 17, true, false, layer);
         map.createFromObjects(layerName, 'Tree-yellow', 'autumn-objects', 21, true, false, layer);
-        map.createFromObjects(layerName, 'Flower-orange-small', 'autumn-objects', 5, true, false, layer);
         map.createFromObjects(layerName, 'Directions-tower', 'autumn-objects', 14, true, false, layer);
         map.createFromObjects(layerName, 'End-tower', 'autumn-objects', 22, true, false, layer);
         map.createFromObjects(layerName, 'Bunny', 'autumn-objects', 23, true, false, layer);
@@ -18,11 +17,6 @@ export default class {
         map.createFromObjects(layerName, 'DoorMask', 'autumn-objects', 29, true, false, layer);
 
         map.createFromObjects(layerName, 'Flower-yellow-small', 'autumn-objects', 13, true, false, layer);
-        map.createFromObjects(layerName, 'Flower-yellow-right-small', 'autumn-objects', 25, true, false, layer);
-        map.createFromObjects(layerName, 'Flower-orangegreen-small', 'autumn-objects', 7, true, false, layer);
-        map.createFromObjects(layerName, 'Flower-orange-right-small', 'autumn-objects', 19, true, false, layer);
-        map.createFromObjects(layerName, 'Little-plant-orange', 'autumn-objects', 18, true, false, layer);
-        map.createFromObjects(layerName, 'Little-plant-yellow', 'autumn-objects', 24, true, false, layer);
         map.createFromObjects(layerName, 'Tree-florished', 'autumn-objects', 28, true, false, layer);
         map.createFromObjects(layerName, 'Tree-green-florished', 'autumn-objects', 34, true, false, layer);
     }
